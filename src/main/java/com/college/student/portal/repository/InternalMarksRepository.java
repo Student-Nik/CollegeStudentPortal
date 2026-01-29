41307327a96fa25016a44edbaf297cdb83d41c72
Nikhil Kute <nkute611@gmail.com>
2026-01-29 09:22:12 +0530

    Enter Internal Marks POST API done...

--- src/main/java/com/college/student/portal/repository/InternalMarksRepository.java
+++ src/main/java/com/college/student/portal/repository/InternalMarksRepository.java
@@ -1,11 +1,17 @@
 package com.college.student.portal.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.college.student.portal.entity.Course;
 import com.college.student.portal.entity.InternalMarks;
+import com.college.student.portal.entity.Student;
 
 @Repository
 public interface InternalMarksRepository extends JpaRepository<InternalMarks, Integer>{
+
+	Optional<InternalMarks> findByStudentAndCourse(Student student, Course course);
 
 }
